7b625364a07c185949114b1a20d4614cb5894df1
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-12 03:54:30 -0300

    set binds

--- regras/cadastrarUsuario.php
+++ regras/cadastrarUsuario.php
@@ -6,15 +6,15 @@ $email = $_POST["email"];
 $senha = $_POST["password"];
 $senha = md5($senha);
 
-$query = "CALL cadastrarUsuario(:nome, :email, :senha)";
+$query = 'CALL cadastrarUsuario(:nome, :email, :senha)';
 // $query = "insert into usuario (nome, email, senha)
 // values ('nome', 'email', 'senha');";
 
 $stmt = $conexao->prepare($query);
 
-$stmt->bindParam(':nome', $nome);
-$stmt->bindParam(':email', $email);
-$stmt->bindParam(':senha', $senha);
+$stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+$stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
 
 $stmt->execute();
 
