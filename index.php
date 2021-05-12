2d6b6e612e3a465681e4419ba54c2f2eb11517ba
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-12 04:21:46 -0300

    set hash password in senha

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 session_start();
-$_SESSION["emailUsuario"]
 if (isset($_SESSION["emailUsuario"])) {
 	header('Location: ./telas/home/home.html');
 }
--- regras/validarUsuario.php
+++ regras/validarUsuario.php
@@ -4,13 +4,14 @@ require_once '../conexao.php';
 try {	
 	$email = $_POST["email"];
 	$senha = $_POST["password"];
+	$senha = md5($senha);
 
 	$query = "SELECT validarUsuario(:email, :senha)";
 
 	$stmt = $conexao->prepare($query);
 
 	$stmt->bindParam(':email', $email, PDO::PARAM_STR);
-	$stmt->bindParam(':senha', md5($senha), PDO::PARAM_STR);
+	$stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
 
 	$rows = $stmt->execute();
 
