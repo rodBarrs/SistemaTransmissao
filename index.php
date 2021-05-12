0b7d1e0eda7acea62f3e648bb4afa90ca5bb9be7
EwertonBello <ewerton.bello1212@gmail.com>
2021-05-12 04:51:00 -0300

    set sessionindex

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
 session_start();
-if (isset($_SESSION["emailUsuario"])) {
-	header('Location: ./telas/home/home.html');
-}
-header('Location: ./telas/login/login.html');
+echo $_SESSION["emailUsuario"];
+// if (isset($_SESSION["emailUsuario"])) {
+// 	header('Location: ./telas/home/home.html');
+// }
+// header('Location: ./telas/login/login.html');
 ?>
